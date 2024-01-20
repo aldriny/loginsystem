8341bf0a2d35238214ed37d0020f6b66934677eb
aldriny <drinymuhammad@gmail.com>
2024-01-20 09:18:07 +0200

    verification commit

--- pages/signup.php
+++ pages/signup.php
@@ -6,18 +6,21 @@
 
     session_start();
 
-    $email = $_POST['email'];
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    $pwd = $_POST['pwd'];
-    $pwdRepeat = $_POST['pwdRepeat'];
-
-    $addUser = new SignupController($email,$firstName,$lastName,$pwd, $pwdRepeat);
-    $addUser->signUp();
+    if(isset($_GET['email']) && isset($_GET['firstName']) && isset($_GET['lastName']) && isset($_GET['pwd']) && isset($_GET['pwdRepeat'])){
+        $email = $_POST['email'];
+        $firstName = $_POST['firstName'];
+        $lastName = $_POST['lastName'];
+        $pwd = $_POST['pwd'];
+        $pwdRepeat = $_POST['pwdRepeat'];
     
-    $_SESSION['email'] = $email;
-    $verifyEmail = new EmailVerificationController();
-    $verifyEmail->sendEmail($email);
-
-    header("location: email_verification.php");
-    exit();
+        $addUser = new SignupController($email,$firstName,$lastName,$pwd, $pwdRepeat);
+        $addUser->signUp();
+        
+        if (isset($_SESSION['email'])) {
+            $_SESSION['email'] = $email;
+            $verifyEmail = new EmailVerificationController();
+            $verifyEmail->sendEmail($email);  
+            header("location: email_verification.php");
+            exit();
+        }
+    }
